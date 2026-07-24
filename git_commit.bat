bee7ba00aec7e4557c0f1392d4ce8dd66ec3054d
Xinglan2012 <3681596340@qq.com>
2026-07-24 11:04:41 +0800

    feat: 欢迎文章置顶(2099年)，抽卡系统新增45张名句+背景图

--- git_commit.bat
+++ git_commit.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 cd /d "C:\Users\何梓熠\Documents\code\xinglan2012.github.io"
 git add -A
-git commit -m "refactor: 冰晶吊坠→碎星之列，金色碎星SVG图标+金色主题"
+git commit -m "feat: 欢迎文章置顶(2099年)，抽卡系统新增45张名句+背景图"
 git push
 echo ✅ 完成
 pause
